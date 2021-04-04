e6e0c3b3213736c51f1d400acc8167efef2f0c82
elpouchkine <fordoxledinodingo@gmail.com>
2021-04-04 15:22:34 +0200

    unDimancheCompliqué

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import * as THREE from '../node_modules/three/build/three.min'
-import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from '/node_modules/three/build/three.min'
+import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
